e5ad023a9b02ccf54c0a0e7e35513c2c0535822e
Jamie <james@areafamily.us>
2022-02-23 20:53:10 -0600

    Added Bonus material for title sections

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -5,7 +5,7 @@ function Default(html) {
   return (
     <html>
     <head>
-      <title>Default</title>
+      <title>{html.title || 'Default'}</title>
     </head>
     <body>
       <h1>HTML Rendered!</h1>
