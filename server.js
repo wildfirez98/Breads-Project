b52bc98933beed87bf4ea0f0b36951f30c20565b
Jamie <james@areafamily.us>
2022-02-23 20:44:56 -0600

    Sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
@@ -22,5 +27,5 @@ app.get('*', (req, res) => {
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
+  console.log('Eating Bread at PORT', PORT);
 })
