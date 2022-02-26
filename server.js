86f3e6a8f0060b8d5a3bf9b4115a475d8a210054
Jamie <james@areafamily.us>
2022-02-26 14:06:04 -0600

    Create and new routes complete

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
+    res.send('<h1>Welcome to an Awesome App about Breads<h1/>')
 })
   
 // Breads endpoint
